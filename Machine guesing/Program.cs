43cf466c3301d57c2fc95c50b815f420a46d33b9
Karel Cveček <Cvecek.Karel@stud.vassboskovice.cz>
2021-11-16 09:30:14 +0100

    Přidejte soubory projektu.

---
+++ Machine guesing/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Machine_guesing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int min = 0;
+            int max = 100;
+            
+           
+            
+        }
+        static void write()
+        { 
+        }
+    }
+}
